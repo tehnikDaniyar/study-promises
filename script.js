8c00521904b20501b2cf0c6927cb1ec0b97b2652
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2023-02-07 22:15:30 +0600

    decided them94 task1

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 import { them94_1 } from "./src/them94/task1.js";
 them94_1();
 
-import { them94_2 } from "./src/them94/task2.js";
-them94_2();
-
-console.log(them94_1() + them94_2());
--- src/them94/task1.js
+++ src/them94/task1.js
@@ -1,2 +1,11 @@
 export const them94_1 = () => {
+	let promise = new Promise(function (resolve) {
+		return setTimeout(() => {
+			resolve("result");
+		}, 10000);
+	});
+
+	promise.then((resolve) => {
+		console.log(resolve);
+	})
 };
