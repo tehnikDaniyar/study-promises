9a3ea8f207b965e9039c0bec3dc251398a495776
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2023-02-07 23:35:35 +0600

    decided them94 task2

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 import { them94_1 } from "./src/them94/task1.js";
 them94_1();
 
+import { them94_2 } from "./src/them94/task2.js";
+them94_2();
--- src/them94/task2.js
+++ src/them94/task2.js
@@ -1,2 +1,34 @@
 export const them94_2 = () => {
+	let promise = new Promise(function (resolve, reject) {
+		let isError = false;
+		if (!isError) {
+			resolve('all right');
+		} else {
+			reject('all bad');
+		}
+	});
+
+
+	promise.then(function (result) {
+		console.log(result);
+	}, function (error) {
+		console.log(error);
+	});
+
+	let generatorNumbers = new Promise(function (resolve, reject) {
+		setInterval(() => {
+			let num = Math.floor(Math.random() * (5 - 0 + 1)) + 0;
+			if (num !== 0) {
+				resolve(1 / num);
+			} else {
+				reject('error, divide on zero is  impossible')
+			}
+		}, 3000);
+	});
+
+	generatorNumbers.then((resolve) => {
+		console.log(resolve);
+	}, (reject) => {
+		console.log(reject);
+	});
 } 
