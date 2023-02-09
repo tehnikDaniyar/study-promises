09e00a1ddeafcd8d9561d9677df72b8a25f5bd81
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2023-02-09 21:58:43 +0600

    decided them96 task1

--- src/them96/task1.js
+++ src/them96/task1.js
@@ -1,3 +1,19 @@
 export const them96_1 = () => {
 
-}
+	function getDelay(min, max) {
+		return Math.round(Math.random() * (max - min + 1) + min)
+	};
+
+	let promises = [];
+
+	for (let i = 0; i < 10; i++) {
+		promises.push(new Promise((resolve) => {
+			let delay = getDelay(1, 10);
+			setTimeout(() => {
+				resolve(`${delay}`);
+			}, delay * 1000);
+		}))
+	};
+
+	Promise.all(promises).then((result) => { console.log(result); })
+};
